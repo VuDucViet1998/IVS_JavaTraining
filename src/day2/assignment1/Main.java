8e77fad6ece66cd9168868bdc2481cc830ee13c2
VietVD3 <VietVu1998@outlook.com>
2023-05-25 22:02:34 +0700

    Added day 2_Update requirements

--- src/day2/assignment1/Main.java
+++ src/day2/assignment1/Main.java
@@ -2,6 +2,9 @@ package day2.assignment1;
 
 import java.util.Scanner;
 public class Main {
+/*    Write a program to print the area of a rectangle by creating a class named 'Area' taking the values
+    of its length and breadth as parameters of its constructor and having a method named 'returnArea'
+    which returns the area of the rectangle. Length and breadth of rectangle are entered through keyboard.*/
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         System.out.print("Enter the length of the rectangle: ");
--- src/day2/assignment2/Main.java
+++ src/day2/assignment2/Main.java
@@ -1,6 +1,8 @@
 package day2.assignment2;
 
 public class Main {
+   /* Write a program that would print the information (name, year of joining, salary, address)
+    of three employees by creating a class named 'Employee'.*/
     public static void main(String[] args) {
         Employee e1 = new Employee("Viet", "2022", "Hung Yen", "0d");
         Employee e2 = new Employee("Viet2", "2022", "Hung Yen", "0d");
